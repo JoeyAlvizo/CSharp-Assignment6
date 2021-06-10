0b63e69e791a22f6012bc5edf221f96edff77fd7
JoeyAlvizo <joey@joeyalvizo.com>
2021-06-10 03:07:52 -0500

    Submit Assignment 6

---
+++ BirthdayCardGenorator/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthdayCardGenorator.Models;
+
+namespace BirthdayCardGenorator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SendCardForm()
+        {
+            return View(new BirthdayCardRequest());
+        }
+        [HttpPost]
+        public IActionResult SendCardForm(Models.BirthdayCardRequest birthdayCardRequest)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("BirthdayCard", birthdayCardRequest);
+            }
+            else
+            {
+                return View(birthdayCardRequest);
+            }
+        }
+    }
+}
---
+++ BirthdayCardGenorator/Models/BirthdayCardRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayCardGenorator.Models
+{
+    public class BirthdayCardRequest
+    {
+        [Required(ErrorMessage = "Please enter From who")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter To who")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter a Message")]
+        public string Message { get; set; }
+    }
+}
